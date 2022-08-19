9cba243a621bb6655f61608e9e043faf3553308a
Robert Ford <robert.ford1996@gmail.com>
2022-08-19 12:09:21 -0400

    finishes lecture properly

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 
 // console.log($.ajax("http://www.omdbapi.com/?i=tt3896198&apikey=7852cb&t=Frozen+2"))
 
-const URL = "http://www.omdbapi.com/?i=tt3896198&apikey=7852cb&t=joker";
+const URL = "http://www.omdbapi.com/?i=tt3896198&apikey=7852cb&t=";
 
 
 //VARIABLES
@@ -15,10 +15,19 @@ const $year = $("#year")
 const $rated = $("#rated")
 const $actors = $("#actors")
 const $awards = $("#awards")
+const $input = $('input[type="text"]')
+const $form = $("form")
 
-function handleGetData(){
 
-$.ajax(URL).then(function(data){
+//EVENT LISTENER
+
+$form.on("submit", handleGetData)
+
+function handleGetData(event){
+    event.preventDefault()
+    userInput = $input.val()
+
+$.ajax(URL+userInput).then(function(data){
     console.log('movie is ready')
     console.log(data)
     $title.text(data.Title)
